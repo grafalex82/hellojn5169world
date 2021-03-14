8b3d068cb727641b7fbad2d8c314c7e3113aa9c5
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2021-03-14 10:44:29 +0200

    Add debug prints

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,7 @@
 #include "AppHardwareApi.h"
+#include "dbg.h"
+#include "dbg_uart.h"
+
 
 #define BOARD_LED_BIT               (17)
 #define BOARD_LED_PIN               (1UL << BOARD_LED_BIT)
@@ -8,6 +11,11 @@
 PUBLIC void vAppMain(void)
 {
 	int i;
+	int iteration = 0;
+	int debugEnabled = 1;
+
+	// Initialize UART
+	DBG_vUartInit(DBG_E_UART_0, DBG_E_UART_BAUD_RATE_115200);
 
 	// Initialize hardware
 	vAHI_DioSetDirection(0, BOARD_LED_CTRL_MASK);
@@ -15,9 +23,11 @@ PUBLIC void vAppMain(void)
 
 	while(1)
 	{
+		DBG_vPrintf(debugEnabled, "Blink iteration %d\n", iteration++);
+
 		vAHI_DioSetOutput(0, BOARD_LED_PIN);
 
-		for(i=0; i<200000; i++)
+		for(i=0; i<1000000; i++)
 		        vAHI_DioSetOutput(0, BOARD_LED_PIN);
 
 		vAHI_DioSetOutput(BOARD_LED_PIN, 0);
@@ -25,7 +35,6 @@ PUBLIC void vAppMain(void)
 		for(i=0; i<1000000; i++)
 		        vAHI_DioSetOutput(BOARD_LED_PIN, 0);
 	}
-
 }
 
 void vAppRegisterPWRMCallbacks(void)
