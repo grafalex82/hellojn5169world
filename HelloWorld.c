5b898ac5926766712023876ab29f2373d312e343
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2021-03-14 16:07:07 +0200

    Blinking via timer (not yet working, missing ISR Handler

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,18 +1,29 @@
 #include "AppHardwareApi.h"
 #include "dbg.h"
 #include "dbg_uart.h"
+#include "ZTimer.h"
 
 
 #define BOARD_LED_BIT               (17)
 #define BOARD_LED_PIN               (1UL << BOARD_LED_BIT)
 #define BOARD_LED_CTRL_MASK         (BOARD_LED_PIN)
 
+ZTIMER_tsTimer timers[1];
+uint8 blinkTimerHandle;
+
+PUBLIC void blinkFunc(void *pvParam)
+{
+//	static int iteration = 0;
+//	DBG_vPrintf(TRUE, "Blink iteration %d\n", iteration++);
+	DBG_vPrintf(TRUE, "Blink iteration\n");
+
+	uint32 currentState = u32AHI_DioReadInput();
+	vAHI_DioSetOutput(currentState^BOARD_LED_PIN, currentState&BOARD_LED_PIN);
+}
 
 PUBLIC void vAppMain(void)
 {
-	int i;
-	int iteration = 0;
-	int debugEnabled = 1;
+	ZTIMER_teStatus status;
 
 	// Initialize UART
 	DBG_vUartInit(DBG_E_UART_0, DBG_E_UART_BAUD_RATE_115200);
@@ -20,25 +31,23 @@ PUBLIC void vAppMain(void)
 	// Initialize hardware
 	vAHI_DioSetDirection(0, BOARD_LED_CTRL_MASK);
 
-	if (bAHI_WatchdogResetEvent())
-	{
-        	DBG_vPrintf(TRUE, "APP: Watchdog timer has reset device!\n");
-	        DBG_vDumpStack();
-	}   
+	DBG_vPrintf(TRUE, "GPIO initialized\n");
+
+	// Init and start timers
+	status = ZTIMER_eInit(timers, sizeof(timers) / sizeof(ZTIMER_tsTimer));
+	DBG_vPrintf(TRUE, "eInit status: %d\n", status);
+	DBG_vPrintf(TRUE, "Array size: %d\n", sizeof(timers) / sizeof(ZTIMER_tsTimer));
+	status = ZTIMER_eOpen(&blinkTimerHandle, blinkFunc, NULL, ZTIMER_FLAG_PREVENT_SLEEP);
+	DBG_vPrintf(TRUE, "eOpen status: %d\n", status);
+	DBG_vPrintf(TRUE, "Handle: %d\n", blinkTimerHandle);
+	status = ZTIMER_eStart(blinkTimerHandle, ZTIMER_TIME_MSEC(100));
+	DBG_vPrintf(TRUE, "eStart status: %d\n", status);
+
+	DBG_vPrintf(TRUE, "Timer initialized\n");
 
 	while(1)
 	{
-		DBG_vPrintf(debugEnabled, "Blink iteration %d\n", iteration++);
-
-		vAHI_DioSetOutput(0, BOARD_LED_PIN);
-
-		for(i=0; i<1000000; i++)
-		        vAHI_DioSetOutput(0, BOARD_LED_PIN);
-
-		vAHI_DioSetOutput(BOARD_LED_PIN, 0);
-
-		for(i=0; i<1000000; i++)
-		        vAHI_DioSetOutput(BOARD_LED_PIN, 0);
+		ZTIMER_vTask();
 
 		vAHI_WatchdogRestart();
 	}
